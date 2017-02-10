e6428006c658fbff4c316cf9d6386ad77d9d51c6
haqueo <omarhaque@hotmail.co.uk>
2017-02-10 13:57:02 +0000

    started solution file

---
+++ ScientificComputation/Projects/project1/solution.py
@@ -0,0 +1,12 @@
+#imports
+import Dijkstra as dijk
+import numpy as np
+
+if __name__ == '__main__':
+
+    # import the rome edges file
+
+    #initialise minutes, minutes where cars are injected
+    minutes = 200
+    injMinutes = 180
+
