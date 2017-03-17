45572bacdbaf53e8ed53e29c30c048999030f6d4
haqueo <omarhaque@hotmail.co.uk>
2017-03-17 11:32:47 +0000

    added some sort of completd version

--- ScientificComputation/Projects/project2/solution.py
+++ ScientificComputation/Projects/project2/solution.py
@@ -33,6 +33,7 @@ def generate_weight_matrix(data):
 
 
 def generate_connectivity(file_name):
+    global completed_before, duration
     # e.g. file_name = './data/jobslist'
     job = []
     duration = []
@@ -94,7 +95,8 @@ def updated_bellman_ford(ist, isp, wei):
             w = wei[u, v]
             if (w != 0):
                 if (d[u] + w < d[v]):
-                    print('graph contains a negative-weight cycle')
+                    #print('graph contains a negative-weight cycle')
+                    pass
 
     # step 4: determine the shortest path
     shpath = [isp]
@@ -105,16 +107,125 @@ def updated_bellman_ford(ist, isp, wei):
 
     return shpath[::-1]
 
+def iterative_bell():
+
+    # initialise data and weight matrix
+    data = generate_connectivity('./data/jobslist')
+    weights = generate_weight_matrix(data)
+    adjusted_weights = -1*weights
+
+    # create a copy of weight matrix
+    temp_weights = adjusted_weights
+
+    # create a list of current nodes
+    current_nodes = range(13)
+
+    # create a list of the longest paths
+    list_of_longest_paths = []
+
+    # iterate until there are no more current nodes
+    while len(current_nodes) != 0:
+
+        # find the longest path
+        path = updated_bellman_ford(26,27,temp_weights)
+        longest_path = path[1:len(path)-1][::2]
+
+        # add this to the list of longest paths
+        list_of_longest_paths.append(longest_path)
+
+        for node in longest_path:
+
+            # remove every node in the list of longest
+            # paths from current_nodes
+            current_nodes.remove(node)
+
+            # update the weight matrix so no nodes
+            # can move to these nodes
+            temp_weights[:,node] = 1
+            temp_weights[:,node] = 1
+
+    return list_of_longest_paths
+
+def iterative_bell2():
+
+    # initialise data and weight matrix
+    data = generate_connectivity('./data/jobslist')
+    weights = generate_weight_matrix(data)
+    adjusted_weights = -1*weights
+
+    # create a copy of weight matrix
+    temp_weights = adjusted_weights
+
+    # create a list of the longest paths
+    list_of_longest_paths = []
+
+    not_reached = range(13)
+
+    while len(not_reached) != 0:
+        # find the shortest path ie the longest
+
+        # find the longest path
+        path = updated_bellman_ford(26,27,temp_weights)
+        longest_path = path[1:len(path)-1][::2]
+        print(longest_path)
+        # add this to the list of longest paths
+        list_of_longest_paths.append(longest_path)
+
+        for i,node in enumerate(longest_path):
+
+            if node in not_reached:
+                not_reached.remove(node)
+
+            if node != longest_path[-1]:
+                temp_weights[node_finish(node),longest_path[i+1]] = 1
+
+    return list_of_longest_paths
+
+def one_step_at_a_time():
+    shortest = [0,1,4]
+
+
+
+
 
 if __name__ == '__main__':
 
-    data = generate_connectivity('./data/jobslist')
+    # data = generate_connectivity('./data/jobslist')
+    #
+    # weights = generate_weight_matrix(data)
+    #
+    # adjusted_weights = -1 * weights
+    #
+    # path = updated_bellman_ford(26, 27, adjusted_weights)
+    # longest_path = path[1:len(path)-1][::2]
+    #
+    # print(longest_path)
 
-    weights = generate_weight_matrix(data)
+    # real question
 
-    adjusted_weights = -1 * weights
 
-    path = updated_bellman_ford(26, 27, adjusted_weights)
-    longest_path = path[1:len(path)-1][::2]
+    paths1 = iterative_bell()
+    final_gantt = np.zeros((13,2),dtype=int)
+    # print(final_gantt)
 
-    print(longest_path)
+    # can_move = [i for i in range(13) if i not in completed_before]
+    # for subpath in paths1:
+    #     if subpath[0] in can_move:
+
+
+    times = [[0,41],[41,92],[0,50],[50,86],[92,130],[21,66],[0,21],[66,98],[0,32],[21,70],[41,71],[70,89],[92,118]]
+    print(times)
+
+    # print(times)
+
+
+    # print(completed_before)
+
+
+
+
+    # try 2
+
+    # print(iterative_bell2())
+
+
